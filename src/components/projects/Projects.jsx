170a19354447589c5df3a34758192d7fa18d901a
shetara2022 <smithshetarall@gmail.com>
2023-03-31 14:20:43 -0400

    added projects component and styling

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,56 +1,59 @@
 import './projects.css'
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container} from 'react-bootstrap'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import trivia  from '../../images/trivia.png'
+import games  from '../../images/games.png'
 
 const Projects = () => {
-
-    const projects = [
-        {
-            title: "Lets Play: Trivia",
-            description: "Trivia game built using html, css, javascript",
-            // imgUrl: projImg1
-        },
-
-        {
-            title: "Lets Play: Trivia",
-            description: "Trivia game built using html, css, javascript",
-            // imgUrl: projImg2
-
-        },
-
-        {
-            title: "Lets Play: Trivia",
-            description: "Trivia game built using html, css, javascript",
-            // imgUrl: projImg3
-        }
-    ]
-
   return (
-      <section className='my-projects'>
+      <section className='my-/projects'>
           <Container>
-              <Row>
-                  <Col>
                       <h2> Projects</h2>
                       <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo facere commodi, enim minima tenetur voluptatibus. Sed officiis obcaecati ducimus cupiditate autem optio distinctio hic, laboriosam fuga consequuntur laudantium nam quae.</p>
 
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="holder.js/100px180" />
-                        <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Text>
-                            Some quick example text to build on the card title and make up the
-                            bulk of the card's content.
-                            </Card.Text>
-                            <Button variant="primary">Go somewhere</Button>
-                        </Card.Body>
-                    </Card>
+                      <div className='card-wrapper'>
+                      <div className='card-container'>
+                      <Card className='pro-card'>
+                            <Card.Img variant="top" src={trivia} />
+                            <Card.Body>
+                                <Card.Title>Let's Play: Trivia</Card.Title>
+                                <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of the card's content.
+                                </Card.Text>
+                                <Button variant="primary">Live Demo</Button>
+                            </Card.Body>
+                            </Card>
+                        </div>
 
-
-                  </Col>
-              </Row>
-          </Container>
-   </section>
+                        <div className='card-container'>
+                            <Card className='pro-card'>
+                            <Card.Img variant="top" src={games} />
+                            <Card.Body>
+                                <Card.Title>Home of the Games</Card.Title>
+                                <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of the card's content.
+                                </Card.Text>
+                                <Button variant="primary">Live Demo</Button>
+                            </Card.Body>
+                            </Card>
+                        </div>
+                  
+                        <div className='card-container'>
+                            <Card className='pro-card'>
+                            <Card.Img variant="top" src="holder.js/100px180" />
+                            <Card.Body>
+                                <Card.Title>Travel & Share Blog</Card.Title>
+                                <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of the card's content.
+                                </Card.Text>
+                                <Button variant="primary">Live Demo</Button>
+                            </Card.Body>
+                            </Card>
+                        </div>
+                        </div>
+            </Container>
+        </section>
   )
 }
 
