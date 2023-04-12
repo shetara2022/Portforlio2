faab54c500b8b3cf808eaf057b80e9f8b4eb5bb3
shetara2022 <smithshetarall@gmail.com>
2023-04-12 13:03:46 -0400

    css animation on pic and name

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -16,11 +16,12 @@ const Banner = () => {
   return (
     <>
       <div className='home' id= 'home'>
-        <div>
+       
+        <TrackVisibility>
+        <div className={isVisible ? "animate__animated animate__fadeInLeft" : ""}>
             <img className='mypic' src={me4} alt="Woman Coder" />
           </div>
-        <TrackVisibility>
-          <div className={isVisible ? "animate__animated animate__rubberBand" : ""}>
+          <div className={isVisible ? "animate__animated animate__lightSpeedInLeft" : ""}>
           <h1 className='intro_words'>
           Hi, I'm Shetara!
               {'  '} </h1> 
@@ -31,7 +32,7 @@ const Banner = () => {
       </TrackVisibility>
     </div>
      <div>
-     <header className='bg text-white p-5' style={{ marginBottom: '10%' }} >
+     <header className='bg text-white p-5' >
           <p>As an experienced educator of 12 years, I have always been passionate about creating engaging and enjoyable lessons that cater to the needs of individual students. During the pandemic, my interest in software development was sparked when I saw the potential for technology to enhance the learning experience. </p>
           <p>My passion for learning is contagious, and I am constantly seeking out new challenges and opportunities for growth. My journey from educator to developer is a testament to my adaptability and tenacity. As a lifelong learner, I am always eager to take on new projects and expand my skill set. My unique combination of skills, dedication, and passion makes me an invaluable asset to any team.</p>
      </header>
